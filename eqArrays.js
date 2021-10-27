69ff3fe2adf7780148dce6e17e545cb25b4f845f
belalelmi <belalelmi@gmail.com>
2021-10-27 00:23:15 -0400

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,8 +8,8 @@ const eqArrays = (a1, b1) => {
     // checking to see if items are not the same throughout the loop
     if (a1[x] !== b1[x]) {
       return false;  // check for item(?) in array1 is not === item(?) in b1
-    };
-  }
+    }
+  };
 
   return true;  // otherwise arrays match
 };

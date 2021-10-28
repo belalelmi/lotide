9b7933a2e247191bf191833dc57415ae05b8dcd0
belalelmi <belalelmi@gmail.com>
2021-10-27 23:59:40 -0400

    find key by object/value

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const findKeyByValue = (object, value) => {
+  const keySearch = Object.keys(object)
+  for (const key of keySearch) {
+    if ((object[key] === value)) {
+      return key;
+    }
+  }
+};
+
+
+const eqArrays = (a1, b1) => {
+  if (a1.length !== b1.length) {
+    return false;
+  }
+  for (let x = 0; x < a1.length; x++) {
+    if (a1[x] !== b1[x]) {
+      return false;
+    }
+  };
+  return true;
+};
+
+const assertArraysEqual = (a1, b1) => {
+  if (eqArrays(a1, b1)) {
+    console.log(`✅ Assertion Passed: ${a1} === ${b1}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${a1} !== ${b1}`);
+  };
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertArraysEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertArraysEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

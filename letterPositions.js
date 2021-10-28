bf97bafd3683710ddf483165a8eab9fe1bab750f
belalelmi <belalelmi@gmail.com>
2021-10-27 22:53:04 -0400

    lindices of each letter in a string

--- letterPositions.js
+++ letterPositions.js
@@ -43,4 +43,4 @@ const assertArraysEqual = (a1, b1) => {
 
 
 // assertArraysEqual(letterPositions("lighthouse abs").l, [0])
-assertArraysEqual(letterPositions("belal")['e'], [1]);
+// assertArraysEqual(letterPositions("belal")['e'], [1]);

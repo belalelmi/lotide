957629b0f7465d3fdd844585291a12c860df855b
belalelmi <belalelmi@gmail.com>
2021-10-29 23:52:01 -0400

    Return subset of array.

--- without.js
+++ without.js
@@ -33,10 +33,3 @@ const without = (source, itemsToRemove) => {
     return newArray;
   };
 };
-
-// without([1, 2, 3], [1]) // => [2, 3]
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);

87b44c65e72d7542a345eb7b877018a9b355214c
belalelmi <belalelmi@gmail.com>
2021-10-29 23:24:28 -0400

    AssertObjectsEquals

---
+++ assertObjectsEqual.js
@@ -0,0 +1,22 @@
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+    return true;
+  }
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};

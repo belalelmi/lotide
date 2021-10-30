ee13cdae275b95733d10f888b3293fcd2c6f9ced
belalelmi <belalelmi@gmail.com>
2021-10-30 18:33:09 -0400

    eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,16 @@
-
 const eqObjects = function (obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   } else {
     for (let i in obj1) {
-      if ((Array.isArray(obj1[i]) && Array.isArray(obj2[i]))) {
-        if (eqArrays(obj1[i], obj2[i]) === false) {
-          return false
-        }
-      } else {
-        if (obj1[i] !== obj2[i]) {
-          return false;
-        }
+      if (obj1[i] !== obj2[i]) {
+        return false;
       }
     }
     return true;
   }
 }
+
 
 
 const assertEqual = function (actual, expected) {

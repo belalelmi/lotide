adb653904499ed92e00b956d2172897e7c37b8d7
belalelmi <belalelmi@gmail.com>
2021-11-01 14:55:29 -0400

    refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('bill', 'bill');
-assertEqual(4, 5);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,22 +1,12 @@
 
+const assertEqual = require('./assertEqual');
+
 const head = (x) => {
   if (x[0] != null || x[0] != undefined) {
-    console.log(x[0]);
+    return x[0];
   } else {
-    console.log('undefined');
+    return undefined;
   }
 };
 
-//test run
-head([4, 5, 6]);
-head(["Hello", "Lighthouse", "Labs"])
-head([]);
-
-
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
+module.exports = head;

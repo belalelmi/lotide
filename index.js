e04ad326620ce393406ae096bea945f290eab435
belalelmi <belalelmi@gmail.com>
2021-11-01 23:12:25 -0400

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const head = require('./head');
 const tail = require('./tail');
-const middle = require('./middle.js');
-
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  eqArrays,
+
 };

c297125bac03e9f203db88800b95ffebbace2a1c
belalelmi <belalelmi@gmail.com>
2021-11-01 22:26:06 -0400

    refactored code with Mocha/Chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,7 +6,7 @@ const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, 3] of [1, 2, 3]", () => {
-    assert.strictEqual((tail(['1', '2', '3'])), ['2', '3']);
+    assert.deepEqual((tail(['1', '2', '3'])), ['2', '3']);
   });
   it("returns '1' for length of tail['5', '6']", () => {
     assert.strictEqual(tail(['5', '6']).length, 1);
